43ca95b4cd792059a14de40c0f4eae4d525d832d
kp3nguinz <justin@penguinzmedia.group>
2021-06-25 07:34:20 +0000

    Alpha

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 apt install python3 python3-pip -y
 pip3 install ansible openshift
 mkdir -p /etc/ansible
+mkdir -p /root/.kube
 FILE=/etc/ansible/hosts
 if test -f "$FILE"; then
     echo "$FILE exists, skipping setup."
@@ -9,4 +10,5 @@ else
 echo "[cluster]"  >> /etc/ansible/hosts
 echo 'localhost ansible_connection=local ansible_python_interpreter="/usr/bin/env python3"'  >> /etc/ansible/hosts
 fi
+export KUBECONFIG=/root/.kube/config
 ansible-playbook  cluster.yaml --connection=local
