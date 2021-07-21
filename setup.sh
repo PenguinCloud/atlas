90279582333a48def9b88d3a143252ce0507904b
kp3nguinz <justin@penguinzmedia.group>
2021-07-21 03:55:29 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
-apt install python3 python3-pip -y
-pip3 install ansible
-ansible-galaxy install -r configs/ansible-requirements.yaml
-mkdir -p /etc/ansible
 mkdir -p /root/.kube
-FILE=/etc/ansible/hosts
-if test -f "$FILE"; then
-    echo "$FILE exists, skipping setup."
-else
-echo "[cluster]"  >> /etc/ansible/hosts
-echo 'localhost ansible_connection=local ansible_python_interpreter="/usr/bin/env python3"'  >> /etc/ansible/hosts
-fi
 export KUBECONFIG=/root/.kube/config
 echo "Printing your local ip info for the next prompt"
 ip a
