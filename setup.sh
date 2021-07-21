77debe829a02cf41841ddb3cd7f59763a3936088
shades <justin@penguinzmedia.group>
2021-07-20 21:12:03 -0700

    fixed some stuff

--- setup.sh
+++ setup.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-mkdir -p /root/.kube
-export KUBECONFIG=/root/.kube/config
-echo "Printing your local ip info for the next prompt"
-ip a
 cd /opt
 git clone git@code.penguinzmedia.group:pmg-build-standards/baseline-ansible-setup.git
 cd baseline-ansible-setup
-/opt/baseline-ansible-setup/setup.sh
+chmod +x ./setup.sh
+./setup.sh
 cd /opt
+
+mkdir -p /root/.kube
+
+export KUBECONFIG=/root/.kube/config
+echo "Printing your local ip info for the next prompt"
+ip a
+
 if [ -z $1 ]; then 
 ansible-playbook  upstart.yaml --connection=local --tags primary,control
 else
