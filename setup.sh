972c6e96897a05322bb3785e97f7db33fc53491a
shades <justin@penguinzmedia.group>
2021-07-27 08:49:58 -0700

    Old code no more no more, hit the road code!

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 mkdir -p /root/.kube
 mkdir -p /opt/atlas
 mkdir -p /opt/baseline-ansible-setup
+
 cd /opt/baseline-ansible-setup
 FILE=/opt/baseline-ansible-setup/setup.sh
 if test -f "$FILE"; then
@@ -29,7 +30,7 @@ cd /opt/atlas
 cp -f /opt/atlas/configs/hosts.yaml /etc/ansible/hosts
 cp -f /opt/atlas/configs/hosts /etc/hosts
 if [ -z $1 ]; then 
-ansible-playbook  upstart.yaml --tags primary,controlcat
+ansible-playbook  upstart.yaml
 else
 ansible-playbook  upstart.yaml --tags $1
 fi
